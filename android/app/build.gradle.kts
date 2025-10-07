9ff91da26536a42d6dd5020c2f6932b1b9e10fae
AhmedElsersy‬‎ <ahmedsersy10@gmail.com>
2025-10-07 23:56:59 +0300

    done

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {        ndkVersion = "29.0.13599879"
-    namespace = "com.skoon.muslim.app"
+    namespace = "com.ghaith.muslim.app"
     compileSdk = flutter.compileSdkVersion
 
 
@@ -23,7 +23,7 @@ android {        ndkVersion = "29.0.13599879"
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.skoon.muslim.app"
+        applicationId = "com.ghaith.muslim.app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 24
